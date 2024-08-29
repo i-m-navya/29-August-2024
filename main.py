84043a9b32e46b77914d6471255eeffa8de4db8e
i-m-navya <navyakhanna.2011@gmail.com>
2024-08-29 16:16:00 +0530

    Today's work

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello")
+def reversestring(s):
+ if len(s) ==1:
+  return s[0]
+ firstchar = s[0]
+ return reversestring(s[1:]) + firstchar
+s = input("Enter a string : ")
+print(reversestring(s))
